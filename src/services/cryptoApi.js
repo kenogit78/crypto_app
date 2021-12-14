2de07de9597038d77bf34ab036c0d298e6259fda
Paul Keno <adarojepaulo@gmail.com>
2021-12-14 13:01:01 +0100

    first commit

---
+++ src/services/cryptoApi.js
@@ -0,0 +1,23 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+const cryptoApiHeaders = {
+    'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+    'x-rapidapi-key': 'e138e013afmshef27749173bd0c7p179082jsnbe83a3737f7b'
+}
+
+const baseUrl = 'https://coinranking1.p.rapidapi.com/';
+
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders });
+
+export const cryptoApi = createApi({
+    reducerPath: 'cryptoApi',
+    baseQuery: fetchBaseQuery({ baseUrl }),
+    endpoints: (builder) => ({
+        getCryptos: builder.query({
+            query: (count) => createRequest(`/coins?limit=${count}`)
+        })
+    })
+});
+
+export const {useGetCryptosQuery} = cryptoApi
+
